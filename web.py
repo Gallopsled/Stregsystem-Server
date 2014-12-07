745b25bdb20b7cc744957432acf636bb6cb38c5e
Ninn <morten@Mortens-MacBook-Pro-3.local>
2014-12-07 01:01:31 +0100

    Adding stuff

--- web.py
+++ web.py
@@ -7,6 +7,76 @@ db_name = "stregliste.db"
 
 
 class Stregliste(object):
+
+	"""
+		Example: action=register_user&params={"cardid": 1234, "username": "Ninn"}
+	"""
+	def handle_create_user(self, params):
+		try:
+			data = json.loads(params)
+		except:
+			return "Could not load json from %s " % params
+
+		for i in ['cardid', 'username']:
+			if not i in data.keys():
+				return "Could not find %s" % i
+
+		try:
+			with sqlite3.connect(db_name) as c:
+				c.execute('INSERT INTO users VALUES (?,?,0)', (data['cardid'], data['username']))
+				c.commit()
+			return json.dumps({'status':"Registration successful"})
+		except:
+			# TODO: Check why it failed?
+			return json.dumps({'status':"Registration failed"})
+
+	"""
+		Example: action=resupply&params={"buyer": "Ninn", "current_stock": 0, "new_stock": 9, "stock_price": 36}
+	"""
+	def handle_resupply(self, params):
+		try:
+			data = json.loads(params)
+		except:
+			return "Could not load json from %s " % params
+
+		for i in ['buyer', 'current_stock', 'new_stock', 'stock_price']:
+			if not i in data.keys():
+				return "Could not find %s" % i
+
+		# TODO: Check if username exists
+		try:
+			with sqlite3.connect(db_name) as c:
+				c.execute('INSERT INTO resupplys VALUES (?,?,?,?)', (data['buyer'], data['current_stock'], data['new_stock'], data['stock_price']))
+				c.commit()
+			return json.dumps({'status':"Resupply successful"})
+		except:
+			# TODO: Check why it failed?
+			return json.dumps({'status':"Resupply failed"})
+
+
+	"""
+		Example: action=buy_one&params={"cardid": "1234"}
+	"""
+	def handle_buy_one(self, params):
+		try:
+			data = json.loads(params)
+		except:
+			return "Could not load json from %s " % params
+
+		for i in ['cardid']:
+			if not i in data.keys():
+				return "Could not find %s" % i
+
+		#try:
+		with sqlite3.connect(db_name) as c:
+			c.execute(' INSERT INTO purchases VALUES (?,1,(select max(rowid) from resupplys))', (data['cardid'],)) #Wired ass comma shit fuck bug wut?
+			c.commit()
+		return json.dumps({'status':"Buy one successful"})
+		#except:
+			# TODO: Check why it failed?
+		#	return json.dumps({'status':"Buy one failed"})
+
+
 	@cherrypy.expose
 	def index(self):
 		return "To do: Implement overview of shit and registration"
@@ -16,36 +86,19 @@ class Stregliste(object):
 		if action == None or params == None:
 			return 'U wot mate?'
 		elif action == "register_user":
-			try:
-				data = json.loads(params)
-			except:
-				return params
-			try:
-				data['cardid']
-			except:
-				return "Cardid is not set"
-
-			try:
-				data['username']
-			except:
-				return "Username is not set"
-			try:
-				with sqlite3.connect(db_name) as c:
-					c.execute('INSERT INTO users VALUES (?,?)', (data['cardid'], data['username']))
-					c.commit()
-				return json.dumps({'status':"Registration successful"})
-			except:
-				# TODO: Check why it failed?
-				return json.dumps({'status':"Registration failed"})
-
-		return "We are yet to implement " + action
-
+			return self.handle_create_user(params)
+		elif action == "resupply":
+			return self.handle_resupply(params)
+		elif action == "buy_one":
+			return self.handle_buy_one(params)
+		else:
+			return "We are yet to implement " + action
 
 
 def setup_database():
 	with sqlite3.connect(db_name) as c:
-		c.execute("CREATE TABLE IF NOT EXISTS users (cardid, username UNIQUE)")
-		c.execute("CREATE TABLE IF NOT EXISTS purchases (cardid, value)")
+		c.execute("CREATE TABLE IF NOT EXISTS users (cardid, username UNIQUE, starting_balance)")
+		c.execute("CREATE TABLE IF NOT EXISTS purchases (cardid, items, supplyid)")
 		c.execute("CREATE TABLE IF NOT EXISTS resupplys (buyer, current_stock, new_stock, stock_price)")
 		c.commit()
 
